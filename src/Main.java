322166db0106d60c6127c26b6530b535c8e3a528
ArturoMandujano <90660839+ArturoMandujano@users.noreply.github.com>
2022-02-18 18:07:27 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 9;
+
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        double f = (double)a/(double)b;
+        int g = a%b;
+
+        System.out.println("El resultado de la suma de estos números es igual a:  "+c);
+        System.out.println("El resultado de la resta de estos números es igual a:  "+d);
+        System.out.println("El resultado de la multiplicación de estos números es igual a:  "+e);
+        System.out.println("El resultado de la división de estos números es igual a:  "+f);
+        System.out.println("El resultado del modulo de estos números es igual a:  "+g);
+    }
+
+}
